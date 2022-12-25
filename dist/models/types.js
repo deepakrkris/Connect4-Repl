633e5c55478e1d1ae5b8ebe06c32895d4dc6886c
Deepak Rajamohan <thangarajamohan@Deepaks-MacBook-Pro.local>
2022-12-25 15:58:55 -0800

    deploy to fly.io

--- dist/models/types.js
+++
@@ -1,9 +0,0 @@
-export function isGameSessionMessage(obj) {
-    return 'type' in obj && obj.type == 'session' && 'userId' in obj && 'gameCode' in obj;
-}
-export function isUserActionMessage(obj) {
-    return 'row' in obj && 'coin' in obj;
-}
-export function isNotificationMessage(obj) {
-    return 'type' in obj && 'message' in obj;
-}
